3f4d4888f3d2b0dc7d9cebaa21a087f3fc214450
Bsting <steve_aw@hotmail.com>
2020-06-17 15:32:43 +0800

    Update trace_testnet.py

--- trace_testnet.py
+++ trace_testnet.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     with torch.no_grad():
         conf = Config()
         model = Backbone(conf.net_depth, conf.drop_ratio, conf.net_mode, conf.net_output)
-        model.load_state_dict(torch.load('model.pth', map_location='cpu'))
+        model.load_state_dict(torch.load('model_test.pth', map_location='cpu'))
         model.eval()
         sample = torch.rand(1,3,112,112)
         traced_script_module = torch.jit.trace(model, sample)
